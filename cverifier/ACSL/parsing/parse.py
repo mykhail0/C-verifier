d5597be9a6965d25b1e89ad94657afea5bdf046d
Mykhailo Shevchenko <mykheilsh@gmail.com>
2021-05-21 00:12:07 +0200

    changed parser a little

--- cverifier/ACSL/parsing/parse.py
+++ cverifier/ACSL/parsing/parse.py
@@ -1,5 +1,6 @@
 import re
 import sys
+import html
 
 def main(path):
     p = re.compile(r'^-{60}\n$')
@@ -53,7 +54,22 @@ def main(path):
                         break
             goals[gl] = (int(re.search(r'\d+', words[words.index('line') + 1]).group()), goal_str, res_mp.get(result, result))
 
-        print(goals)
+        s, e = 0, 0
+        print('<pre>')
+        for i, line in enumerate(lines):
+            if i == int_lines[0][s]:
+                print('<button onclick="$(document).ready(() -> {{$("#goal{}").toggle();}});">Hide/Unhide</button>'.format(i))
+                print('<div id="goal{}"'.format(i))
+                s += 1
+                if s >= len(int_lines[0]):
+                    s = 0
+            print(html.escape(line), end='')
+            if i == int_lines[1][e]:
+                print('</div>')
+                e += 1
+                if e >= len(int_lines[1]):
+                    e = 0
+        print('</pre>')
 
 if __name__ == "__main__":
     args = sys.argv[1:]
